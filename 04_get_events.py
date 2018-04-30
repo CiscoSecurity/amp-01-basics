aa7b652880c90f03903dabcca5551b7eb825c5b4
Unknown <michael@threatgrid.com>
2018-04-30 11:27:43 -0400

    Initial Scripts

---
+++ 04_get_events.py
@@ -0,0 +1,11 @@
+import requests
+
+client_id = 'a1b2c3d4e5f6g7h8i9j0'
+
+api_key = 'a1b2c3d4-e5f6-g7h8-i9j0-k1l2m3n4o5p6'
+
+url = 'https://{}:{}@api.amp.cisco.com/v1/events'.format(client_id,api_key)
+
+r = requests.get(url)
+
+print r.json()
