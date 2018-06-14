b00a2350165dc23021b93925313f455f58666520
Unknown <michael@threatgrid.com>
2018-06-14 11:21:31 -0400

    Update authentication
    Switched from using URL based auth to using HTTPBasiAuth function

--- 02_get_computers.py
+++ 02_get_computers.py
@@ -4,8 +4,8 @@ client_id = 'a1b2c3d4e5f6g7h8i9j0'
 
 api_key = 'a1b2c3d4-e5f6-g7h8-i9j0-k1l2m3n4o5p6'
 
-url = 'https://{}:{}@api.amp.cisco.com/v1/computers'.format(client_id,api_key)
+url = 'https://api.amp.cisco.com/v1/computers'
 
-r = requests.get(url)
+r = requests.get(url, auth=(client_id, api_key))
 
 print r.json()
